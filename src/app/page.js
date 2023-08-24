241423b65b05fbaf2ab7d2a89e9bee9cfe79f91b
Chayanin <arm.crazyplay@gmail.com>
2023-08-24 10:23:30 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import { Container, Text, Title } from "@mantine/core";
+
+export default function FoodReviewPage() {
+  return (
+    <Container size="600px">
+      <Title order={2}>Food Review 🍕</Title>
+
+      <Text align="center" color="dimmed" my="sm">
+        Copyright © 2023 Chayanin Suatap 650610560
+      </Text>
+    </Container>
+  );
+}
